b3ca799b76567b08c029c966cff42858e32b045e
Alex Yang <ahy3nz@virginia.edu>
2019-04-16 12:33:42 -0500

    Reorganizing, overlay court plotting func

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(name='ballDontLie',
+        version='0.0',
+        description='Various NBA curiosities in Python',
+        author='Alex Yang',
+        author_email='alexander.h.yang@vanderbilt.edu',
+        packages=find_packages()
+        )
