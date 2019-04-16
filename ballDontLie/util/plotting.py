87bb17d2d89a2565dc809ffc265dc93c119701b8
Alex Yang <ahy3nz@virginia.edu>
2019-04-16 13:16:32 -0500

    Court shapes for plotting in plotly

--- ballDontLie/util/plotting.py
+++ ballDontLie/util/plotting.py
@@ -1,6 +1,6 @@
 from matplotlib.patches import Circle, Rectangle, Arc
 
-def overlay_court(ax=None, color='black', lw=2, outer_lines=False):
+def overlay_court_mpl(ax=None, color='black', lw=2, outer_lines=False):
     """ Credit tohttp://savvastjortjoglou.com/nba-shot-sharts.html """
     # If an axes object isn't provided to plot onto, just get current one
     if ax is None:
@@ -68,3 +68,228 @@ def overlay_court(ax=None, color='black', lw=2, outer_lines=False):
         ax.add_patch(element)
 
     return ax
+
+def overlay_court_ply():
+    """ Credit to 
+    https://moderndata.plot.ly/nba-shots-analysis-using-plotly-shapes/"""
+    court_shapes = []
+
+    outer_lines_shape = dict(
+        type='rect',
+        xref='x',
+        yref='y',
+        x0='-250',
+        y0='-47.5',
+        x1='250',
+        y1='422.5',
+        line=dict(
+            color='rgba(10, 10, 10, 1)',
+            width=1
+            )
+    )
+
+    court_shapes.append(outer_lines_shape)
+
+    hoop_shape = dict(
+        type='circle',
+        xref='x',
+        yref='y',
+        x0='7.5',
+        y0='7.5',
+        x1='-7.5',
+        y1='-7.5',
+        line=dict(
+            color='rgba(10, 10, 10, 1)',
+            width=1
+        )
+    )
+
+    court_shapes.append(hoop_shape)
+
+    backboard_shape = dict(
+        type='rect',
+        xref='x',
+        yref='y',
+        x0='-30',
+        y0='-7.5',
+        x1='30',
+        y1='-6.5',
+        line=dict(
+            color='rgba(10, 10, 10, 1)',
+            width=1
+        ),
+        fillcolor='rgba(10, 10, 10, 1)'
+    )
+
+    court_shapes.append(backboard_shape)
+
+    outer_three_sec_shape = dict(
+        type='rect',
+        xref='x',
+        yref='y',
+        x0='-80',
+        y0='-47.5',
+        x1='80',
+        y1='143.5',
+        line=dict(
+            color='rgba(10, 10, 10, 1)',
+            width=1
+        )
+    )
+
+    court_shapes.append(outer_three_sec_shape)
+
+    inner_three_sec_shape = dict(
+        type='rect',
+        xref='x',
+        yref='y',
+        x0='-60',
+        y0='-47.5',
+        x1='60',
+        y1='143.5',
+        line=dict(
+            color='rgba(10, 10, 10, 1)',
+            width=1
+        )
+    )
+
+    court_shapes.append(inner_three_sec_shape)
+
+    left_line_shape = dict(
+        type='line',
+        xref='x',
+        yref='y',
+        x0='-220',
+        y0='-47.5',
+        x1='-220',
+        y1='92.5',
+        line=dict(
+            color='rgba(10, 10, 10, 1)',
+            width=1
+        )
+    )
+
+    court_shapes.append(left_line_shape)
+
+    right_line_shape = dict(
+        type='line',
+        xref='x',
+        yref='y',
+        x0='220',
+        y0='-47.5',
+        x1='220',
+        y1='92.5',
+        line=dict(
+            color='rgba(10, 10, 10, 1)',
+            width=1
+        )
+    )
+
+    court_shapes.append(right_line_shape)
+
+
+    three_point_arc_shape = dict(
+        type='path',
+        xref='x',
+        yref='y',
+        path='M -220 92.5 C -70 300, 70 300, 220 92.5',
+        line=dict(
+            color='rgba(10, 10, 10, 1)',
+            width=1
+        )
+    )
+
+    court_shapes.append(three_point_arc_shape)
+    three_point_arc_shape = dict(
+        type='path',
+        xref='x',
+        yref='y',
+        path='M -220 92.5 C -70 300, 70 300, 220 92.5',
+        line=dict(
+            color='rgba(10, 10, 10, 1)',
+            width=1
+        )
+    )
+
+    court_shapes.append(three_point_arc_shape)
+
+
+    center_circle_shape = dict(
+        type='circle',
+        xref='x',
+        yref='y',
+        x0='60',
+        y0='482.5',
+        x1='-60',
+        y1='362.5',
+        line=dict(
+            color='rgba(10, 10, 10, 1)',
+            width=1
+        )
+    )
+
+    court_shapes.append(center_circle_shape)
+    center_circe_shape = dict(
+        type='circle',
+        xref='x',
+        yref='y',
+        x0='60',
+        y0='482.5',
+        x1='-60',
+        y1='362.5',
+        line=dict(
+            color='rgba(10, 10, 10, 1)',
+            width=1
+        )
+    )
+
+    court_shapes.append(center_circle_shape)
+    res_circle_shape = dict(
+        type='circle',
+        xref='x',
+        yref='y',
+        x0='20',
+        y0='442.5',
+        x1='-20',
+        y1='402.5',
+        line=dict(
+            color='rgba(10, 10, 10, 1)',
+            width=1
+        )
+    )
+
+    court_shapes.append(res_circle_shape)
+
+    free_throw_circle_shape = dict(
+        type='circle',
+        xref='x',
+        yref='y',
+        x0='60',
+        y0='200',
+        x1='-60',
+        y1='80',
+        line=dict(
+            color='rgba(10, 10, 10, 1)',
+            width=1
+        )
+    )
+
+    court_shapes.append(free_throw_circle_shape)
+
+    res_area_shape = dict(
+        type='circle',
+        xref='x',
+        yref='y',
+        x0='40',
+        y0='40',
+        x1='-40',
+        y1='-40',
+        line=dict(
+            color='rgba(10, 10, 10, 1)',
+            width=1,
+            dash='dot'
+        )
+    )
+
+    court_shapes.append(res_area_shape)
+    return court_shapes
